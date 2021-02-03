86ab1b72096a2a58e18c6c0e6c41036ba28b7dda
Gobind Puniani <gobindpuniani@gmail.com>
2021-02-03 15:45:10 -0800

    Initial Commit

---
+++ SwiftUI Calculator/SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalculatorApp.swift
+//  SwiftUI Calculator
+//
+//  Created by Gobind Puniani on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
